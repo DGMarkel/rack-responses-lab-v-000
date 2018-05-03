53db62c7ae21a9b572ecca6896639a1bb8a55081
David Markel <davidgmarkel@gmail.com>
2018-05-03 16:29:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,8 @@
 class Application
+  attr_accessor :t_statement
 
   def call(env)
     resp = Rack::Response.new
-
-    t = Time.now
-
-    if t.min < 10
-      time_now = "#{t.hour}:0#{t.min}"
-    else
-      time_now = "#{t.hour}:#{t.min}"
-    end
-
-    t_statement = "The time is now #{time_now}"
 
     if t.hour < 12
       resp.write "Good Morning! #{t_statement}"
